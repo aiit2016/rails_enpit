5d580096290de472e0b7c6bf2f589955656ad260
Koala Cheung <a1624jz@aiit.ac.jp>
2016-09-23 00:12:16 +0000

    Modify rakuten item ranking page

--- app/controllers/rakuten_item_ranking_controller.rb
+++ app/controllers/rakuten_item_ranking_controller.rb
@@ -3,17 +3,12 @@ require 'rakuten_web_service'
 
 class RakutenItemRankingController < ApplicationController
   def index
-    #RakutenWebService.configuration do |c|
     RakutenWebService.configure do |c|
       c.application_id = ENV["RAPI_APPID"]
       c.affiliate_id = ENV["RAPI_AFID"]
-puts c.application_id
-puts 'c.application_id'
-puts c.affiliate_id
     end
 
     # Use genre id to fetch genre object
     @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 40, :sex => 0)
-    #erb :rakuten_item_ranking
   end
 end
