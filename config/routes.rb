8fbb86cfc7b462ec4c241d60b8c9db8161694dae
Aritomo Moritoh <z14010am@aiit.ac.jp>
2016-09-24 08:54:57 +0000

    kuma

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'rakuten_item_ranking' => 'rakuten_item_ranking#index'
-
+  get 'kumamon' => 'kumamon#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
